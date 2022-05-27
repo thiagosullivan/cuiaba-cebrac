9ec6dc2b352d30acbb22dbb126651a6b863b92d8
thiagosullivan <thiagosullivan12@gmail.com>
2022-05-26 23:20:46 -0300

    Adding facebook pixel

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,27 @@ import { ThemeProvider } from 'styled-components';
 import theme from '../styles/theme';
 import { Toaster } from 'react-hot-toast';
 import '../styles/globals.css';
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+const FB_PIXEL_ID = process.env.REFRESH_TOKEN;
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    import('react-facebook-pixel')
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init(FB_PIXEL_ID) // facebookPixelId
+        ReactPixel.pageView()
+
+        router.events.on('routeChangeComplete', () => {
+          ReactPixel.pageView()
+        })
+      })
+  }, [router.events])
+
   return (
     <ThemeProvider theme={theme}>
       <Toaster position="bottom-right" />
