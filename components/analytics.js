a8f199f6851970bb47ad3e743901466019e713db
thiagosullivan <thiagosullivan12@gmail.com>
2022-05-30 07:42:37 -0300

    correction for deploy

--- components/analytics.js
+++ components/analytics.js
@@ -5,9 +5,12 @@ const Analytics = () => (
   <>
     <Script
       strategy="afterInteractive"
-      src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
       id="gtm-script"
-    />
+    >
+      {
+        `https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`
+      }
+    </Script>
     <Script
       strategy="afterInteractive"
       dangerouslySetInnerHTML={{
