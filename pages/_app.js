9b301619aa58d3fa2280c6755808220ab0bb50c6
thiagosullivan <thiagosullivan12@gmail.com>
2022-05-30 17:10:08 -0300

    Changing facebook pixel ID

--- pages/_app.js
+++ pages/_app.js
@@ -6,11 +6,12 @@ import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import * as gtag from './../lib/gtag';
 import Analytics from './../components/analytics';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
 
-  const FB_PIXEL = process.env.PIXEL_FB;
+  // const FB_PIXEL = process.env.PIXEL_FB;
 
   useEffect(() => {
     const handleRouteChange = url => {
@@ -22,18 +23,32 @@ function MyApp({ Component, pageProps }) {
     }
   }, [router.events])
 
-  useEffect(async () => {
-    const { default: ReactPixel } = await import('react-facebook-pixel');
-    ReactPixel.init(FB_PIXEL, null, {
-        autoConfig: true,
-        debug: true,
-      });
-    ReactPixel.pageView();
-    ReactPixel.track("ViewContent")
-  });
+  // useEffect(async () => {
+  //   const { default: ReactPixel } = await import('react-facebook-pixel');
+  //   ReactPixel.init(FB_PIXEL, null, {
+  //       autoConfig: true,
+  //       debug: true,
+  //     });
+  //   ReactPixel.pageView();
+  //   ReactPixel.track("ViewContent")
+  // });
 
   return (
     <>
+      <Script id="facebook-pixel">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '557013655243129');
+          fbq('track', 'PageView');
+        `}
+      </Script>
       <ThemeProvider theme={theme}>
         <Toaster position="bottom-right" />
         <Component {...pageProps} />    
